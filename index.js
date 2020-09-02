d07db27d542feadc3cc15ce200b871178c3c2d35
HCgx3 <65537022+HCgx3@users.noreply.github.com>
2020-09-02 19:19:59 +0100

    a

--- index.js
+++ index.js
@@ -9,52 +9,52 @@ bot.aliases = new Discord.Collection();
 const { Player } = require("discord-player");
 
 // To easily access the player
-bot.player = player;
-const player = new Player(client, { 
-    leaveOnEmpty: true, 
-    leaveOnEnd: false, 
-    leaveOnStop: true 
-  });
 
+const player = new Player(client, {
+    leaveOnEmpty: true,
+    leaveOnEnd: false,
+    leaveOnStop: true
+});
+bot.player = player;
 
 
 fs.readdir("./commands/", (err, files) => {
 
     if (err) console.log(err)
-  
+
     let jsfile = files.filter(f => f.split(".").pop() === "js")
     if (jsfile.length <= 0) {
-      return console.log("[LOGS] Couldn't Find Commands!");
+        return console.log("[LOGS] Couldn't Find Commands!");
     }
-  
+
     jsfile.forEach((f, i) => {
-      let pull = require(`./commands/${f}`);
-      bot.commands.set(pull.config.name, pull);
-      pull.config.aliases.forEach(alias => {
-        bot.aliases.set(alias, pull.config.name)
-      });
+        let pull = require(`./commands/${f}`);
+        bot.commands.set(pull.config.name, pull);
+        pull.config.aliases.forEach(alias => {
+            bot.aliases.set(alias, pull.config.name)
+        });
     });
-  });
+});
 
 
-  bot.on("message", async message => {
+bot.on("message", async message => {
     if (message.author.bot || message.channel.type === "dm") return;
     if (!message.channel.permissionsFor(bot.user.id).has("SEND_MESSAGES")) return;
     let botprefix = 'm!'
-        if (message.content.match(new RegExp(`^<@!?${bot.user.id}>( |)$`))) {
-          return message.channel.send(`${message.guild.name}'s Prefix is \`${botprefix}\`\nUse \`${botprefix}help\` for a the help page.`)
-            }
-        if(message.content.indexOf(botprefix) !== 0) return;
-        let messageArray = message.content.split(" ");
-        let cmd = messageArray[0];
-        let args = messageArray.slice(1)
-  
-        if (!message.content.startsWith(botprefix)) return;
-        let commandfile = bot.commands.get(cmd.slice(botprefix.length)) || bot.commands.get(bot.aliases.get(cmd.slice(botprefix.length)))
-        if (commandfile) commandfile.run(bot, message, args, track)
-  
-  
-          })
+    if (message.content.match(new RegExp(`^<@!?${bot.user.id}>( |)$`))) {
+        return message.channel.send(`${message.guild.name}'s Prefix is \`${botprefix}\`\nUse \`${botprefix}help\` for a the help page.`)
+    }
+    if (message.content.indexOf(botprefix) !== 0) return;
+    let messageArray = message.content.split(" ");
+    let cmd = messageArray[0];
+    let args = messageArray.slice(1)
+
+    if (!message.content.startsWith(botprefix)) return;
+    let commandfile = bot.commands.get(cmd.slice(botprefix.length)) || bot.commands.get(bot.aliases.get(cmd.slice(botprefix.length)))
+    if (commandfile) commandfile.run(bot, message, args, track)
+
+
+})
 
 
 
@@ -62,5 +62,5 @@ fs.readdir("./commands/", (err, files) => {
 
 
 bot.login(process.env.Token)
-    
+
 
